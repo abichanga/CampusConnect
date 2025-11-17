9249801093d8df5fb11804c3236194389e0341f7
Jude <jude.makau@strathmore.edu>
2025-11-17 15:07:18 +0300

    Remove large files from history using BFG

--- app/src/main/java/com/example/mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/mobile_app/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobile_app.auth.ui.AuthRoute
 import com.example.mobile_app.ui.theme.MobileappTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +14,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobileappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CampusConnectApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileappTheme {
-        Greeting("Android")
-    }
+fun CampusConnectApp() {
+    AuthRoute()
 }
